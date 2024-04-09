c041c4c7315a346c30fbcfe60be19df5c4432ea4
Stanislas MEZUREUX <stanislas.mezureux@telecomnancy.eu>
2024-04-09 11:35:03 +0200

    [FEAT] ping sweep subnet auto

--- src/scanner/ip_scan/ip_scan.c
+++ src/scanner/ip_scan/ip_scan.c
@@ -5,6 +5,9 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <string.h>
+#include "../../../libs/log/log.h"
 
 void ping_sweep(in_addr_t ip_addrs[], int len)
 {
@@ -39,4 +42,67 @@ void ping_sweep(in_addr_t ip_addrs[], int len)
     {
         waitpid(child_pids[i], NULL, 0);
     }
+}
+
+void _ping_sweep_subnet(in_addr_t network, in_addr_t mask)
+{
+    // Compute the number of hosts in the subnet
+    in_addr_t broadcast = network | ~mask;
+    int num_hosts = ntohl(broadcast) - ntohl(network) + 1;
+
+    // Allocate memory for the array of IP addresses
+    in_addr_t *ip_addrs = malloc((num_hosts - 2) * sizeof(in_addr_t));
+    if (!ip_addrs)
+    {
+        fprintf(stderr, "Failed to allocate memory\n");
+        return;
+    }
+
+    // Generate the list of IP addresses in the subnet
+    int j = 0;
+    for (int i = 1; i < num_hosts - 1; i++, j++)
+    {
+        ip_addrs[j] = (network & mask) + htonl(i);
+    }
+
+    // Call the ping_sweep function with the list of IP addresses
+    ping_sweep(ip_addrs, num_hosts - 2);
+
+    free(ip_addrs);
+}
+
+void ping_sweep_subnet(void)
+{
+    struct ifaddrs *ifap, *ifa;
+    int found = 0;
+    getifaddrs(&ifap);
+    for (ifa = ifap; ifa; ifa = ifa->ifa_next)
+    {
+        if (ifa->ifa_addr && ifa->ifa_addr->sa_family == AF_INET)
+        {
+            // Skip loopback interface
+            if (strcmp(ifa->ifa_name, "lo") == 0 || strcmp(ifa->ifa_name, "lo0") == 0)
+            {
+                continue;
+            }
+            // Found non-loopback interface
+            if (!found)
+            {
+                struct sockaddr_in *sa = (struct sockaddr_in *)ifa->ifa_addr;
+                log_info("Interface: %s", ifa->ifa_name);
+                log_info("IP address: %s", inet_ntoa(sa->sin_addr));
+                log_info("Netmask: %s", inet_ntoa(((struct sockaddr_in *)ifa->ifa_netmask)->sin_addr));
+                _ping_sweep_subnet(sa->sin_addr.s_addr, ((struct sockaddr_in *)ifa->ifa_netmask)->sin_addr.s_addr);
+                found = 1;
+                break;
+            }
+        }
+    }
+    freeifaddrs(ifap);
+}
+
+int main(void)
+{
+    ping_sweep_subnet();
+    return 0;
 }
--- src/scanner/ip_scan/ip_scan.h
+++ src/scanner/ip_scan/ip_scan.h
@@ -6,9 +6,22 @@
 /**
  * @brief Ping a target_host with ICMP protocol
  *
- * @param target_host IP address of the host to ping
+ * @param ip_addrs IP address of the host to ping
  * @param len Length of the ip_addrs array
  */
 void ping_sweep(in_addr_t ip_addrs[], int len);
+
+/**
+ * @brief Ping a subnet with ICMP protocol
+ *
+ * @param network IP address of the network to ping
+ * @param mask Subnet mask of the network to ping
+ */
+void _ping_sweep_subnet(in_addr_t network, in_addr_t mask);
+
+/**
+ * @brief Ping a subnet with ICMP protocol using the default gateway
+ */
+void ping_sweep_subnet(void);
 
 #endif /* IP_SCAN_H */
